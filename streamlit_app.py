1329f4445b417c2ac58f29c75d402bbb9d071f1b
Mellymel-appdev4ever <115200132+badgesnowflake@users.noreply.github.com>
2022-10-07 10:30:30 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -7,3 +7,5 @@ streamlit.text('🥣 Omega 3 & blueberry Oatmeal')
 streamlit.text('🥗 Kale, Spinach & Rocket Smoothie')
 streamlit.text('🐔 Hard-Boiled Free-Range Egg')
 streamlit.text('🥑 🍞 Avocado Toast')
+
+streamlit.header('🍌🍓 Buld Your Own Fruit Snoothie🥝🍇')

eea19144b439ae4c45939234607d914b5fac6fe1
Mellymel-appdev4ever <115200132+badgesnowflake@users.noreply.github.com>
2022-10-07 09:50:48 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,4 +2,4 @@ import streamlit
 
 streamlit.title('My Parents New Healthy Diner')
 
-streamlit.title('Breakfast Menu')'<p 'font-size:20px;" </p>'
+streamlit.title('Breakfast Menu')'<p style='font-size:20px;" </p>'

ff0ec383730bd7959833dabb132dab95cca52269
Mellymel-appdev4ever <115200132+badgesnowflake@users.noreply.github.com>
2022-10-07 10:16:13 -0500

    Update streamlit_app.py
    Update the menu

--- streamlit_app.py
+++ streamlit_app.py
@@ -5,5 +5,5 @@ streamlit.title('My Parents New Healthy Diner')
 streamlit.header('Breakfast Menu')
 
 streamlit.text('Omega 3 & blueberry Oatmeal\n')
-streamlit.text('Omega 3 & blueberry Oatmeal\n')
-streamlit.text('Omega 3 & blueberry Oatmeal\n')
+streamlit.text('Kale, Spinach & Rocket Smoothie\n')
+streamlit.text('Hard-Boiled Free-Range Egg\n')

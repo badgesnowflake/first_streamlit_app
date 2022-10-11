9348a617afa26b020cc96b4c47dbba713f9dd1bc
Mellymel-appdev4ever <115200132+badgesnowflake@users.noreply.github.com>
2022-10-11 13:45:04 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -31,18 +31,24 @@ fruits_to_show = my_fruit_list.loc[fruits_selected]
 #streamlit.dataframe(my_fruit_list)
 streamlit.dataframe(fruits_to_show)
 
+# New Section to display fruityvice api response
 streamlit.header("Fruityvice Fruit Advice!")
-fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + "kiwi")
-fruit_choice = streamlit.text_input('What fruit would you like information about?','Kiwi')
-streamlit.write('The user entered ', fruit_choice)
-
-
+try:
+   fruit_choice = streamlit.text_input('What fruit would you like information about?')
+   if not fruit_choice:
+        streamlit.error("Please select a fruit to get information. ")
+      else:
+        fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice)
+        # normaized the data using pandas 
+        fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
+        streamlit.write('The user entered ', fruit_choice)
+        # display the result in streamlit
+        streamlit.dataframe(fruityvice_normalized)
+except URLError as e:
+   streamlit.error()
+    
+    
 #streamlit.text(fruityvice_response.json())
-
-# normaized the data using pandas 
-fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
-# display the result in streamlit
-streamlit.dataframe(fruityvice_normalized)
 # don't run anything past here while we troubleshoot
 streamlit.stop()
 my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])

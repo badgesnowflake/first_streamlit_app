63de2cdaa22092b906c2e1f37e6f5f1648f5b5ad
Mellymel-appdev4ever <115200132+badgesnowflake@users.noreply.github.com>
2022-10-11 14:04:39 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -31,6 +31,13 @@ fruits_to_show = my_fruit_list.loc[fruits_selected]
 #streamlit.dataframe(my_fruit_list)
 streamlit.dataframe(fruits_to_show)
 
+# Create the repeatable code blick (called a function)
+def get_fruityvice_data(this_fruit_choice):
+   fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + this_fruit_choice)
+   fruityvice_normalized = pandas.json_normalized(fruityvice_response.json())
+   return fruityvice_normalized
+
+
 # New Section to display fruityvice api response
 streamlit.header("Fruityvice Fruit Advice!")
 try:
@@ -38,14 +45,10 @@ try:
    if not fruit_choice:
         streamlit.error("Please select a fruit to get information. ")
    else:
-        fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice)
-        # normaized the data using pandas 
-        fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
-        streamlit.write('The user entered ', fruit_choice)
+        back_from_function = get_fruityvice_data(fruit_choice)
         # display the result in streamlit
-        streamlit.dataframe(fruityvice_normalized)
-except URLError as e:
-   streamlit.error()
+        streamlit.dataframe(back_from_function)
+
     
     
 #streamlit.text(fruityvice_response.json())
